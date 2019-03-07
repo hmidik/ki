c8c8321470bbc5e7a81158e89bdcb8aedd1422be
Karim Hmidi <khmidi@centos7.fritz.box>
2019-03-07 13:32:38 +0100

    Code formatting.

--- src/gateway.c
+++ src/gateway.c
@@ -10,50 +10,50 @@
  */
 void handle_communication(void)
 {
-	uint8_t const *modem_data_buff;
-	size_t modem_data_length = 0;
-	wireless_message_t wireless_msg;
-	modem_message_t modem_msg;
+  uint8_t const *modem_data_buff;
+  size_t modem_data_length = 0;
+  wireless_message_t wireless_msg;
+  modem_message_t modem_msg;
 
-	// Packet coming in over the modem
-	if(modem_dequeue_incoming( &modem_data_buff, &modem_data_length))
-	{
-		memcpy(&modem_msg, modem_data_buff, modem_data_length);
+  // Packet coming in over the modem
+  if(modem_dequeue_incoming( &modem_data_buff, &modem_data_length))
+  {
+    memcpy(&modem_msg, modem_data_buff, modem_data_length);
 
-		// Check if the command ment to be executed by the gateway?
-		if(modem_msg.flags & GATEWAY_COMMAND_FLAG)
-		{
-			switch (modem_msg.ctl_cmd)
-			{
-				case PING:
-					// Send an alive signal!
-          memset(&modem_data_buff, 0, MODEM_MAX_PAYLOAD_LENGTH);
-					memcpy(&modem_data_buff, "PONG", sizeof("PONG"));
-					modem_enqueue_outgoing(modem_data_buff, sizeof(modem_data_buff));
-					break;
+    // Check if the command ment to be executed by the gateway?
+    if(modem_msg.flags & GATEWAY_COMMAND_FLAG)
+    {
+      switch (modem_msg.ctl_cmd)
+    	{
+        case PING:
+    		 // Send an alive signal!
+        memset(&modem_data_buff, 0, MODEM_MAX_PAYLOAD_LENGTH);
+    		memcpy(&modem_data_buff, "PONG", sizeof("PONG"));
+    		modem_enqueue_outgoing(modem_data_buff, sizeof(modem_data_buff));
+    		break;
 
-				case RESTART:
-				  // Restart gateway!
-					reset_device();
-					break;
-
-				default:
-					break;
+        case RESTART:
+    		// Restart gateway!
+    	  reset_device();
+    		break;
+        
+    		default:
+    		break;
       }
-		}
+    }
     // The command will be treated by the sensor
-		else if(modem_msg.flags & SENSOR_COMMAND_FLAG)
-		{
-			device_id_t to_sensor_id;
-			memcpy(to_sensor_id.bytes, modem_msg.sensor_id, sizeof(modem_msg.sensor_id));
-			wireless_msg.ctl_cmd = modem_msg.ctl_cmd;
-			wireless_msg.flags = modem_msg.flags;
-			wireless_msg.wi_data_length = sizeof(modem_msg.token);
-			memcpy(wireless_msg.token, modem_msg.token, sizeof(modem_msg.token));
+    else if(modem_msg.flags & SENSOR_COMMAND_FLAG)
+    {
+      device_id_t to_sensor_id;
+      memcpy(to_sensor_id.bytes, modem_msg.sensor_id, sizeof(modem_msg.sensor_id));
+      wireless_msg.ctl_cmd = modem_msg.ctl_cmd;
+      wireless_msg.flags = modem_msg.flags;
+      wireless_msg.wi_data_length = sizeof(modem_msg.token);
+      memcpy(wireless_msg.token, modem_msg.token, sizeof(modem_msg.token));
 
       // Send to sensor
-			wireless_enqueue_outgoing(to_sensor_id, wireless_msg.wi_msg);
-		}
+      wireless_enqueue_outgoing(to_sensor_id, wireless_msg.wi_msg);
+    }
     // Invalid command
     else
     {
@@ -61,7 +61,7 @@ void handle_communication(void)
       memcpy(&modem_data_buff, "ERROR", sizeof("ERROR"));
       modem_enqueue_outgoing(modem_data_buff, sizeof(modem_data_buff));
     }
-	}
+  }
 
   device_id_t from_sensor_id;
   uint8_t wireless_data_buff[WIRELESS_PAYLOAD_LENGTH];
